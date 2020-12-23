0361f8d230b5ef15516da35458bebc579ca36845
Marcos Contreras Luis <mcontrerasluis@gmail.com>
2020-12-23 14:50:37 -0600

    Se cambia el contexto

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -98,7 +98,7 @@ module.exports = (options) => ({
             chunks: ['polyfills', 'main', 'global'],
             chunksSortMode: 'manual',
             inject: 'body',
-            base: '/',
+            base: '/RFCSolZnFr/',
         }),
         new AngularCompilerPlugin({
             mainPath: utils.root('src/main/webapp/app/app.main.ts'),
